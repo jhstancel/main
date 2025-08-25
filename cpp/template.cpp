6edc3c258a2bdbbf2780d1dbe9392e477aea57b3
jhstancel <jhstancel@gmail.com>
2025-08-25 14:05:20 -0400

    added template file

---
+++ cpp/template.cpp
@@ -0,0 +1,25 @@
+/*
+    A header comment must be included in each source file. A header comment should contain the following:
+
+    Your name (username):
+    Date submitted:
+    Lab section:
+    Assignment name:
+
+    Description: (only if this is the file containing the main() function). Give a brief description of what the program does. The description should make sense to anyone who wants to understand the main points of your program. Do not include information about how the program works
+
+     
+    Function Comments: Include header documentation for each function. The header documentation should be placed in comments directly above the function header. A brief description of the function is usually sufficient.
+
+     
+    Statements: Diagnostic / debug prints should be disabled/deleted in the final submission.
+
+     
+    Names: Always assume that someone else will be reading your code; therefore, do not use obscure names for variables, structs/classes, or functions.
+
+     
+    Style and Formatting:
+        Indentation should be consistent throughout the code and follow standard conventions. Avoid mixing spaces and tabs for indentation. Additionally blank lines should be used between function definitions and anywhere else as necessary to clearly separate different parts of the source file.
+        If a function exceeds 50-100 statements in length control structures and/or helper functions should be implemented to reduce the length of the function. Likewise very long statements should span multiple lines (should not exceed 80-120 characters per line). Ideally the entire function definition should be viewable in one screen.
+        Avoid declaring additional global variables (certain global variables from starter code are needed for test cases), however you are free to declare any additional global constants.
+*/
